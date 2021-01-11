97fff598e80ff700073dc40933606c348c404868
yar <ahmadyar228@gmail.com>
2021-01-11 12:52:44 +0500

    Divide func

--- part_1.py
+++ part_1.py
@@ -1,7 +1,10 @@
 def calculator(x, y, operation):
     if operation == '+':
         return x+y
+    elif operation == '/':
+        return x/y
     else:
         print('Not allowed')
 
 print(calculator(1, 2, '+'))
+print(calculator(1, 2, '/'))
